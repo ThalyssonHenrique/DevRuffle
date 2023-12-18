5f665a7241b97bdeb8af7f82835bfe9fcd017e81
ThalyssonHenrique <thalyshenriique@gmail.com>
2023-12-18 14:52:56 -0300

    project finish

---
+++ script.js
@@ -0,0 +1,14 @@
+const buttonDraw = document.querySelector(".draw-button")
+
+function resultRuffle() {
+    const inputValueMin = Math.ceil(document.querySelector(".input-between").value)
+    const inputValueMax = Math.floor(document.querySelector(".input-and").value)
+
+    const result = Math.floor(Math.random() * (inputValueMax - inputValueMin + 1)) + inputValueMin;
+
+    alert(result)
+
+    
+}
+
+buttonDraw.addEventListener("click", resultRuffle)

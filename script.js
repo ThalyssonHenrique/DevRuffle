0aa579979e610ef9b5726a9b9c498342e9d2b0b6
ThalyssonHenrique <thalyshenriique@gmail.com>
2023-12-19 09:48:46 -0300

    add >= no input e alteração da cor do button

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const buttonDraw = document.querySelector(".draw-button")
+buttonDraw.addEventListener("click", resultRuffle)
 
 function resultRuffle() {
     const inputValueMin = Math.ceil(document.querySelector(".input-between").value)
     const inputValueMax = Math.floor(document.querySelector(".input-and").value)
-
     const result = Math.floor(Math.random() * (inputValueMax - inputValueMin + 1)) + inputValueMin;
 
-    alert(result)
+    if(inputValueMin >= inputValueMax){
+        alert("The value on the right needs to be higher")
+    }
+    else{
+        alert(result)
+    }
+
+    
 }
 
-buttonDraw.addEventListener("click", resultRuffle)
+
